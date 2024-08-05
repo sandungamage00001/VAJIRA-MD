8ca798864c4577645fac3d2104e2353cd51b1e7f
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-08-05 20:05:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,14 +8,14 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94728756898' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '94754015815' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=SVMA0aKJ#95wGG74VRIVD8ZoezhuDUMK5MRrjv9ilw8bwOWK74t4' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=iItQ1C7J#brlpuOJRiQkHd3tgnw-RM7erAsp4xYwOCvNo9v0Unz4' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
-OWNER_NAME: process.env.OWNER_NAME || 'KAWINDA' ,
+OWNER_NAME: process.env.OWNER_NAME || 'CHARITH_KAWINDA' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'nonbutton' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
