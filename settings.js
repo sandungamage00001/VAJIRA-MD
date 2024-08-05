7f1e79b611411ebeb48bfa0200632efa967c7bb5
sandungamage00001 <155751506+sandungamage00001@users.noreply.github.com>
2024-08-05 19:51:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94728756898' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=TFdEEI7D#HaQUojI_iubQOI7Xvji8Mu1_KMZ5MzkZxuRF48JwztM' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=SVMA0aKJ#95wGG74VRIVD8ZoezhuDUMK5MRrjv9ilw8bwOWK74t4' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
